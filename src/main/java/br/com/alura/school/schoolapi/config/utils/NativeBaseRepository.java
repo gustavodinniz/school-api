462e7f158beaa5cab6fb12da40541c9dcf8f00e5
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-20 09:47:51 -0300

    ajuste classe NativeBaseRepository

--- src/main/java/br/com/alura/school/schoolapi/config/utils/NativeBaseRepository.java
+++ src/main/java/br/com/alura/school/schoolapi/config/utils/NativeBaseRepository.java
@@ -2,46 +2,16 @@ package br.com.alura.school.schoolapi.config.utils;
 
 import lombok.extern.java.Log;
 import org.sql2o.Connection;
-import org.sql2o.Query;
 import org.sql2o.Sql2o;
 import org.sql2o.quirks.PostgresQuirks;
 
 import javax.sql.DataSource;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Log
 public abstract class NativeBaseRepository {
-
-    protected void executeNativeScript(String sql) {
-        executeNativeScript(sql, new HashMap<>());
-    }
-
-    protected void executeNativeScript(String sql, Map<String, Object> params) {
-        Sql2o sql2o = new Sql2o(getDataSource(), new PostgresQuirks());
-        Connection con = null;
-
-        try {
-
-            con = sql2o.open();
-            Query query = con.createQuery(sql);
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                query.addParameter(entry.getKey(), entry.getValue());
-            }
-
-            query.executeUpdate();
-            con.commit();
-
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (Throwable e) {
-                    log.warning("Error on closing database connection");
-                }
-            }
-        }
-    }
 
     public abstract DataSource getDataSource();
 
@@ -60,61 +30,6 @@ public abstract class NativeBaseRepository {
             return query.executeAndFetch(returnType);
         }
     }
-
-    protected <X> X parseNativeQueryToSingleResult(String sqlQuery, Class<X> returnType) {
-        return parseNativeQueryToSingleResult(sqlQuery, returnType, new HashMap<>());
-    }
-
-    protected <X> X parseNativeQueryToSingleResult(String sqlQuery, Class<X> returnType, Map<String, Object> params) {
-        Sql2o sql2o = new Sql2o(getDataSource(), new PostgresQuirks());
-
-        try (Connection con = sql2o.open()) {
-            org.sql2o.Query query = con.createQuery(sqlQuery);
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                query.addParameter(entry.getKey(), entry.getValue());
-            }
-            return query.executeAndFetchFirst(returnType);
-        }
-    }
-
-    public String replaceParams(String sqlQuery, Map<String, Object> params) {
-        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        final Map<String, Object> treeMap = new TreeMap<>(
-                (current, next) -> {
-                    if (current.length() > next.length()) {
-                        return -1;
-                    }
-
-                    if (current.length() < next.length()) {
-                        return 1;
-                    }
-                    return current.compareTo(next);
-                }
-        );
-        treeMap.putAll(params);
-
-        for (Map.Entry<String, Object> entry : treeMap.entrySet()) {
-            if (entry.getValue() instanceof Date) {
-                sqlQuery = sqlQuery.replaceAll(String.format(":%s", entry.getKey()), String.format("'%s'", dateFormat.format(entry.getValue())));
-                continue;
-            }
-
-            if (entry.getValue() instanceof Number) {
-                sqlQuery = sqlQuery.replaceAll(String.format(":%s", entry.getKey()), entry.getValue() + "");
-                continue;
-            }
-
-            if (entry.getValue() instanceof String) {
-                sqlQuery = sqlQuery.replaceAll(String.format(":%s", entry.getKey()), String.format("'%s'", entry.getValue()));
-                continue;
-            }
-
-            sqlQuery = sqlQuery.replaceAll(String.format(":%s", entry.getKey()), String.valueOf(entry.getValue()));
-        }
-
-        return sqlQuery;
-    }
-
 
 
 }
