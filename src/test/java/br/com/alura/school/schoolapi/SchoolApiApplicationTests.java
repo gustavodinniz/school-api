406fc96261486ee20dd51bb74813b4a173550939
gustavodiniz <gustavo.diniz@tangerino.com.br>
2021-12-19 22:36:59 -0300

    initial project

---
+++ src/test/java/br/com/alura/school/schoolapi/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.school.schoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
